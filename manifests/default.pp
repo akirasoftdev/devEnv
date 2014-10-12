b9d9525fd3a10775176c511f2f5c76b50c16b375
Akira <xMihamaMadao@gmail.com>
2014-10-12 11:58:28 +0900

    冗長なファイルのダウンロードをしなようにした tar.gzの展開を追加

--- manifests/default.pp
+++ manifests/default.pp
@@ -2,21 +2,45 @@
 package {"lubuntu-desktop": ensure => "installed", install_options => ['--no-install-recommends']}
 
 # Android Studioのダウンロード
-exec {"/opt/android-studio-bundle-135.1339820-linux.tgz":
+exec {"download_android_studio":
 	command => "wget https://dl.google.com/android/studio/install/0.8.6/android-studio-bundle-135.1339820-linux.tgz",
 	path => "/usr/bin",
 	cwd => "/opt/",
 	user => "root",
 	group => "root",
-	timeout => 0
+	timeout => 0,
+	unless => "/usr/bin/test -e /opt/android-studio-bundle-135.1339820-linux.tgz"
 }
 
 # Eclipseをダウンロード
-exec {"/opt/eclipse-java-luna-SR1-linux-gtk-x86_64.tar.gz":
+exec {"download_eclipse":
 	command => "wget http://ftp.yz.yamagata-u.ac.jp/pub/eclipse//technology/epp/downloads/release/luna/SR1/eclipse-java-luna-SR1-linux-gtk-x86_64.tar.gz",
 	path => "/usr/bin",
 	cwd => "/opt/",
 	user => "root",
 	group => "root",
-	timeout => 0
+	timeout => 0,
+	unless => "/usr/bin/test -e /opt/eclipse-java-luna-SR1-linux-gtk-x86_64.tar.gz"
+}
+
+# android studioの展開
+exec {"extract_android_studio_tar_ball":
+	command => "tar zxvf android-studio-bundle-135.1339820-linux.tgz",
+	path => "/bin",
+	cwd => "/opt/",
+	user => "root",
+	group => "root",
+	require => Exec["download_android_studio"],
+	unless => "/usr/bin/test -d /opt/android-studio"
+}
+
+# eclipseの展開
+exec {"extract_eclipse_tar_ball":
+	command => "tar zxvf eclipse-java-luna-SR1-linux-gtk-x86_64.tar.gz",
+	path => "/bin",
+	cwd => "/opt/",
+	user => "root",
+	group => "root",
+	require => Exec["download_eclipse"],
+	unless => "/usr/bin/test -d /opt/eclipse"
 }
