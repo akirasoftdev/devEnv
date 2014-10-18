f7e3d13863065f8184bf6b6fdf0f522b43107bb8
Akira <xMihamaMadao@gmail.com>
2014-10-18 10:40:43 +0900

    disable logoutput.

--- manifests/classes/android-studio.pp
+++ manifests/classes/android-studio.pp
@@ -2,7 +2,7 @@ class android-studio {
 	# Android Studioのダウンロード
 	exec {"download_android_studio":
 		command => "wget https://dl.google.com/android/studio/install/0.8.6/android-studio-bundle-135.1339820-linux.tgz",
-		logoutput => true,
+		logoutput => false,
 		timeout => 0,
 		path => "/usr/bin",
 		cwd => "/opt/",
@@ -14,7 +14,7 @@ class android-studio {
 	# android studioの展開
 	exec {"extract_android_studio_tar_ball":
 		command => "tar zxvf android-studio-bundle-135.1339820-linux.tgz",
-		logoutput => true,
+		logoutput => false,
 		timeout => 0,
 		path => "/bin",
 		cwd => "/opt/",
