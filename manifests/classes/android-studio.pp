8273f7c25949be43884a10daddcc012d245282f3
Akira <xMihamaMadao@gmail.com>
2014-12-10 09:03:30 +0900

    changed android-studio version.

--- manifests/classes/android-studio.pp
+++ manifests/classes/android-studio.pp
@@ -1,23 +1,48 @@
 # encoding: UTF-8
 
 class android-studio {
-	# Android Studioのダウンロード
-	exec {'download_android_studio':
-		command => 'wget https://dl.google.com/android/studio/install/0.8.6/android-studio-bundle-135.1339820-linux.tgz',
+	package {"unzip": ensure => present} ->
+	package {"lib32stdc++6": ensure => present} ->
+	package {"lib32z1": ensure => present} ->
+
+	exec {"download_android_studio":
+		command => "wget https://dl.google.com/dl/android/studio/ide-zips/1.0.0/android-studio-ide-135.1629389-linux.zip",
 		logoutput => false,
 		timeout => 0,
-		path => '/usr/bin',
-		cwd => '/opt/',
-		unless => 'test -e /opt/android-studio-bundle-135.1339820-linux.tgz'
+		path => "/usr/bin",
+		cwd => "/opt/",
+		unless => "test -e /opt/android-studio-ide-135.1629389-linux.zip"
 	} ->
 
-	# android studioの展開
-	exec {'extract_android_studio_tar_ball':
-		command => 'tar zxvf android-studio-bundle-135.1339820-linux.tgz',
+	exec {"extract_android_studio_tar_ball":
+		command => "unzip android-studio-ide-135.1538390-linux.zip",
 		logoutput => false,
 		timeout => 0,
-		path => '/bin',
-		cwd => '/opt/',
-		unless => '/usr/bin/test -d /opt/android-studio'
+		path => "/bin:/usr/bin",
+		cwd => "/opt/",
+		unless => "test -d /opt/android-studio"
+	} ->
+	
+	exec {"download android sdk":
+		command => "wget https://dl.google.com/android/adt/adt-bundle-linux-x86_64-20140702.zip",
+		logoutput => false,
+		timeout => 0,
+		path => "/usr/bin",
+		cwd => "/opt",
+		unless => "test -e /opt/adt-bundle-linux-x86_64-20140702.zip"
+	} ->
+	
+	exec {"extract android sdk":
+		command => "unzip adt-bundle-linux-x86_64-20140702.zip",
+		logoutput => false,
+		timeout => 0,
+		path => "/bin:/usr/bin",
+		cwd => "/opt/",
+		unless => "test -d /opt/adt-bundle-linux-x86_64-20140702"
+	} ->
+	
+	file {"/opt/android-studio/sdk":
+		ensure => link,
+		target => "/opt/adt-bundle-linux-x86_64-20140702/sdk"
 	}
 }
